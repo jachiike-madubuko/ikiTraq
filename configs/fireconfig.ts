a5c74ddabc5eaea1a27bd1405b0719750b9fd193
Jachiike <jachiike.madubuko@gmail.com>
2020-07-24 16:41:42 -0500

    configs added

---
+++ configs/fireconfig.ts
@@ -0,0 +1,10 @@
+export const config = {
+    apiKey: "AIzaSyBxBIsFsxJM25WgT8feHsVoBDhHHUfBGb8",
+    authDomain: "ikitraq.firebaseapp.com",
+    databaseURL: "https://ikitraq.firebaseio.com",
+    projectId: "ikitraq",
+    storageBucket: "ikitraq.appspot.com",
+    messagingSenderId: "518210269276",
+    appId: "1:518210269276:web:ae17f3c2849e3202b37699",
+    measurementId: "G-9RGRCTD3B2"
+};
